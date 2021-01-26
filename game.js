11062873c69780b7674dbca7ddb184e247adc739
emmafewer <emmafewer20@gmail.com>
2021-01-26 12:01:03 -0800

    close but have a bug with asteroids

--- game.js
+++ game.js
@@ -3,6 +3,10 @@ const S_SIZE = 30 //ship size
 const SHIP_ACC = 5 //acceleration
 const R_SPEED = 420 //rotation speed
 const FR = 0.5 //friction
+const ASTEROID_NUM = 5 // max starting number of asteroids
+const ASTEROID_SPEED = 50 // max starting asteroid speed in pixels per second
+const ASTEROID_SIZE = 100
+const ASTEROIDS_VERT = 10 // avg num of vertices on each asteroid
 
 // gets canvas elements for js use
 let c = document.getElementById("gameScreen")
@@ -22,6 +26,8 @@ let ship = {
    }
 }
 
+
+
 //sets our canvas size to the current screen size
 c.width = window.innerWidth
 c.height = window.innerHeight
@@ -29,6 +35,8 @@ c.height = window.innerHeight
 //use for menue elements to run the game
 const runGame = () => {
    shipListeners()
+   let asteroids = []
+   createAsteroids()
    setInterval(renderGame, 1000 / FPS)
 }
 
@@ -38,6 +46,7 @@ const renderGame = () => {
    ctx.fillRect(0, 0, c.width, c.height)
    
    renderShip()
+   renderAsteroids()
 }
 
 //draws the ship and calls function to handle movement
@@ -90,6 +99,64 @@ const moveShip = () => {
       ship.y = 0 - ship.r
    }
 
+}
+
+//asteroids function
+const createAsteroids = () => {
+   asteroids = []
+   let x, y
+   for (var i = 0; i < ASTEROID_NUM; i++) {
+      x = Math.floor(Math.random() * c.width)
+      y = Math.floor(Math.random() * c.height)
+      asteroids.push(newAsteroid(x, y))
+   }
+}
+
+const newAsteroid = (x, y) => {
+   let asteroid = {
+      x: x,
+      y: y,
+      xv: Math.random() * ASTEROID_SPEED / FPS * (Math.random () < 0.5 ? 1 : -1),
+      yv: Math.random() * ASTEROID_SPEED / FPS * (Math.random () < 0.5 ? 1 : -1),
+      r: ASTEROID_SIZE / 2,
+      a: Math.random() * Math.PI * 2, // in radians
+      vert: Math.floor(Math.random() * (ASTEROIDS_VERT + 1) + ASTEROIDS_VERT / 2)
+   }
+   return asteroid
+}
+
+//draws the asteroids
+const renderAsteroids = () => {
+   ctx.strokeStyle = "grey"
+   ctx.lineWidth = S_SIZE / 20
+   let x, y, r, a, vert
+   for (let i = 0; i < asteroids.length; i++) {
+      //get the asteroid properties
+      x = asteroids[i].x
+      y = asteroids[i].y
+      r = asteroids[i].r
+      a = asteroids[i].a
+      vert = asteroids[i].vert
+
+      //draw path
+      ctx.beginPath()
+      ctx.moveTo(
+         x + r * Math.cos(a),
+         y + r * Math.sin(a)
+      )
+   }
+      // draw polygon
+      for (var j = 0; j < vert; j++) {
+         ctx.lineTo(
+            x + r * Math.cos(a + j * Math.PI * 2 / vert),
+            y + r * Math.sin(a + j * Math.PI * 2 / vert)
+         )
+      }
+      ctx.closePath()
+      ctx.stroke()
+      //move asteroid
+
+      //handle edge of screen
 }
 
 // listens for key presses
